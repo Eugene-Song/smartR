2ea117d6e1be7ac4401a097491803bdf4c06f6d8
Yuxin Song <ys385@duke.edu>
2023-06-09 13:05:19 +0800

    before clean code

--- src/main/resources/static/admin/dist/js/product.js
+++ src/main/resources/static/admin/dist/js/product.js
@@ -1,29 +1,82 @@
+// $(function () {
+//     $("#jqGrid").jqGrid({
+//         url: '/admin/products/list',
+//         datatype: "json",
+//         colModel: [
+//             {label: '商品ID', name: 'productID', index: 'productID', width: 50, key: true},
+//             {label: '商品名称', name: 'productName', index: 'productName', width: 140},
+//             {label: '商品组ID', name: 'productGroupID', index: 'productGroupID', width: 120},
+//             {label: '商品组名称', name: 'productGroupName', index: 'productGroupName', width: 60},
+//             {label: '销售状态', name: 'salesStatus', index: 'salesStatus', width: 60},
+//             {label: '开发员工工号', name: 'developerEmployeeNumber', index: 'developerEmployeeNumber', width: 60},
+//             {label: '开发员工姓名', name: 'productDeveloper', index: 'productDeveloper', width: 90},
+//             {label: '上架日期', name: 'listingDate', index: 'listingDate', width: 90},
+//             {label: '市场ID', name: 'productMarketID', index: 'productMarketID', width: 90},
+//             {label: '市场唯一ID', name: 'marketUniqueID', index: 'marketUniqueID', width: 90},
+//             {label: '供应商ID', name: 'supplierID', index: 'supplierID', width: 90},
+//             {label: '店铺ID', name: 'shopID', index: 'shopID', width: 90},
+//         ],
+//         height: 700,
+//         rowNum: 10,
+//         rowList: [10, 20, 50],
+//         styleUI: 'Bootstrap',
+//         loadtext: '信息读取中...',
+//         rownumbers: false,
+//         rownumWidth: 20,
+//         autowidth: true,
+//         multiselect: true,
+//         pager: "#jqGridPager",
+//         jsonReader: {
+//             root: "data.list",
+//             page: "data.currPage",
+//             total: "data.totalPage",
+//             records: "data.totalCount"
+//         },
+//         prmNames: {
+//             page: "page",
+//             rows: "limit",
+//             order: "order",
+//         },
+//         gridComplete: function () {
+//             //隐藏grid底部滚动条
+//             $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
+//         }
+//     });
+//
+//     $(window).resize(function () {
+//         $("#jqGrid").setGridWidth($(".card-body").width());
+//     });
+// });
+
+
 $(function () {
     $("#jqGrid").jqGrid({
         url: '/admin/products/list',
         datatype: "json",
         colModel: [
-            {label: '商品ID', name: 'productID', index: 'productID', width: 50, key: true},
-            {label: '商品名称', name: 'productName', index: 'productName', width: 140},
-            {label: '商品组ID', name: 'productGroupID', index: 'productGroupID', width: 120},
-            {label: '商品组名称', name: 'productGroupName', index: 'productGroupName', width: 60},
-            {label: '销售状态', name: 'salesStatus', index: 'salesStatus', width: 60},
-            {label: '开发员工工号', name: 'developerEmployeeNumber', index: 'developerEmployeeNumber', width: 60},
-            {label: '开发员工姓名', name: 'productDeveloper', index: 'productDeveloper', width: 90},
-            {label: '上架日期', name: 'listingDate', index: 'listingDate', width: 90},
-            {label: '市场ID', name: 'productMarketID', index: 'productMarketID', width: 90},
-            {label: '市场唯一ID', name: 'marketUniqueID', index: 'marketUniqueID', width: 90},
-            {label: '供应商ID', name: 'supplierID', index: 'supplierID', width: 90},
-            {label: '店铺ID', name: 'shopID', index: 'shopID', width: 90},
+            {label: '商品ID', name: 'productID', index: 'productID', width: 200, key: true},
+            {label: '商品名称', name: 'productName', index: 'productName', width: 200},
+            {label: '商品组ID', name: 'productGroupID', index: 'productGroupID', width: 200},
+            {label: '商品组名称', name: 'productGroupName', index: 'productGroupName', width: 200},
+            {label: '销售状态', name: 'salesStatus', index: 'salesStatus', width: 200},
+            {label: '开发员工工号', name: 'developerEmployeeNumber', index: 'developerEmployeeNumber', width: 200},
+            {label: '开发员工姓名', name: 'productDeveloper', index: 'productDeveloper', width: 200},
+            {label: '上架日期', name: 'listingDate', index: 'listingDate', width: 200},
+            {label: '市场ID', name: 'productMarketID', index: 'productMarketID', width: 200},
+            {label: '市场唯一ID', name: 'marketUniqueID', index: 'marketUniqueID', width: 200},
+            {label: '供应商ID', name: 'supplierID', index: 'supplierID', width: 200},
+            {label: '店铺ID', name: 'shopID', index: 'shopID', width: 200},
         ],
         height: 700,
         rowNum: 10,
+        width: $(".card-body").width(),
         rowList: [10, 20, 50],
         styleUI: 'Bootstrap',
         loadtext: '信息读取中...',
         rownumbers: false,
         rownumWidth: 20,
-        autowidth: true,
+        autowidth: false, // Changed this to false
+        shrinkToFit: false, // Added this line
         multiselect: true,
         pager: "#jqGridPager",
         jsonReader: {
@@ -38,8 +91,7 @@ $(function () {
             order: "order",
         },
         gridComplete: function () {
-            //隐藏grid底部滚动条
-            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "scroll"}); // Changed this from "hidden" to "scroll"
         }
     });
 
@@ -47,6 +99,7 @@ $(function () {
         $("#jqGrid").setGridWidth($(".card-body").width());
     });
 });
+
 
 /**
  * 搜索功能
